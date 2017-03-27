3c81fff4195aecdad8a7afc05062f12a7eba60b2
Lucija M <meglalucija@gmail.com>
2017-03-27 13:21:17 +0200

    Question 1.7 solved

--- src/cracking/the/code/inerview/utilitiles/Matrix.java
+++ src/cracking/the/code/inerview/utilitiles/Matrix.java
@@ -4,6 +4,8 @@ import java.util.Random;
 
 public class Matrix {
 
+	private static final int min = 0;
+	private static final int max = 50;
 	private int size;
 	private int[][] matrix;
 
@@ -12,12 +14,22 @@ public class Matrix {
 		this.matrix = generateRandomMatrix(size);
 	}
 
+	public Matrix(int[][] matrix, int size) {
+		this.size = size;
+		this.matrix = new int[size][size];
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				this.matrix[i][j] = matrix[i][j];
+			}
+		}
+	}
+
 	private int[][] generateRandomMatrix(int size) {
-		Random generator = new Random();
+		Random rand = new Random();
 		int[][] randomMatrix = new int[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				randomMatrix[i][j] = generator.nextInt();
+				randomMatrix[i][j] = rand.nextInt((max - min) + 1) + min;
 			}
 		}
 		return randomMatrix;
@@ -27,9 +39,25 @@ public class Matrix {
 		return size;
 	}
 
+	public int[][] getMatrix() {
+		return matrix;
+	}
+
 	public int getElement(int i, int j) {
 		checkIndices(i, j);
 		return matrix[i][j];
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				sb.append(matrix[i][j]);
+				sb.append(" ");
+			}
+			sb.append(System.lineSeparator());
+		}
+		return sb.toString();
 	}
 
 	private void checkIndices(int i, int j) {
--- src/cracking/the/code/interview/arrays_and_strings/Question1_7.java
+++ src/cracking/the/code/interview/arrays_and_strings/Question1_7.java
@@ -1,5 +1,66 @@
 package cracking.the.code.interview.arrays_and_strings;
 
+import java.util.Scanner;
+
+import cracking.the.code.inerview.utilitiles.Matrix;
+
+/**
+ * Rotate matrix
+ * 
+ * @author Lucija Megla
+ *
+ */
 public class Question1_7 {
+	/**
+	 * In the problem, we see that each pixel needs 4 bytes, that means that we
+	 * should use an integer matrix, so we could store each pixel in an element
+	 * in matrix. The best way is to perform swaps layer-wise. Each matrix NxN
+	 * matrix has N/2 layers.
+	 * 
+	 * If we are doing layer-wise swaps, we need to determine 'beginning' and
+	 * 'ending' of each layer (first and last in method rotateMatrix).
+	 * 
+	 * When we rotate elements in one layer, we need to keep track of offset
+	 * which shows us what elements we are swaping.
+	 * 
+	 * Memory: O(1), time: O(n^2), because we need to iterate over all matrix
+	 * elements.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out
+				.println("Please enter matrix size. Random matrix will be generated.");
+		Scanner scan = new Scanner(System.in);
+		int size = scan.nextInt();
+		scan.close();
+		Matrix matrix = new Matrix(size);
+		System.out.println(matrix.toString());
+
+		int[][] matrixArr = matrix.getMatrix();
+
+		rotateMatrix(matrixArr, size); // inplace swap
+
+		System.out.println(matrix.toString());
+
+	}
+
+	private static void rotateMatrix(int[][] matrixArr, int size) {
+
+		for (int layer = 0; layer < size / 2; layer++) {
+			int first = layer;
+			int last = size - 1 - layer;
+			for (int i = first; i < last; i++) {
+				int offset = i - first;
+
+				int temp = matrixArr[first][i];
+				matrixArr[first][i] = matrixArr[last - offset][first];
+				matrixArr[last - offset][first] = matrixArr[last][last - offset];
+				matrixArr[last][last - offset] = matrixArr[i][last];
+				matrixArr[i][last] = temp;
+			}
+		}
+	}
 
 }
