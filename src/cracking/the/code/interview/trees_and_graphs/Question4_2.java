b8a4343223384154770c26bc9b567cc2bbc12e3b
Lucija M <meglalucija@gmail.com>
2017-11-06 07:37:00 +0100

    Comment added

--- src/cracking/the/code/interview/trees_and_graphs/Question4_2.java
+++ src/cracking/the/code/interview/trees_and_graphs/Question4_2.java
@@ -4,29 +4,40 @@ import cracking.the.code.interview.utilitiles.TreeNode;
 
 public class Question4_2 {
 
-	static int[] arr = {1,2,3,4,5,6,7,8,9};
-	
+	static int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
 	public static void main(String[] args) {
-		
+
 		TreeNode root = createMinBST(arr, 0, arr.length - 1);
-		
+
 		printBST(root);
-		
+
 	}
-	
+
+	// extra memory because of recursion
+	// time complexity O(n) - 
 	public static TreeNode createMinBST(int[] arr, int startIndex, int endIndex) {
-		if(startIndex > endIndex) {
+		if (startIndex > endIndex) { // base case
 			return null;
 		}
-		int rootIndex = startIndex + (endIndex - startIndex)/2;
-		TreeNode left = createMinBST(arr, startIndex, rootIndex - 1);
-		TreeNode right = createMinBST(arr, rootIndex + 1,  endIndex);
+		int rootIndex = (endIndex + startIndex) / 2;
+		TreeNode left = createMinBST(arr, startIndex, rootIndex - 1); // with
+																		// each
+																		// recursion
+																		// we
+																		// send
+																		// half
+																		// of
+																		// the
+																		// leftover
+																		// array
+		TreeNode right = createMinBST(arr, rootIndex + 1, endIndex);
 		TreeNode root = new TreeNode(arr[rootIndex], left, right);
 		return root;
 	}
-	
+
 	public static void printBST(TreeNode root) {
-		if(root == null) {
+		if (root == null) {
 			return;
 		}
 		printBST(root.getLeft());
