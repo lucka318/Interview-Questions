f5d03ef19a098a0e8612a3aa49dea4b875068ae2
Lucija M <meglalucija@gmail.com>
2017-11-20 08:00:09 +0100

    Question 4.7 solved, Question 4.8 unfinished

--- src/cracking/the/code/interview/trees_and_graphs/Question4_7.java
+++ src/cracking/the/code/interview/trees_and_graphs/Question4_7.java
@@ -1,7 +1,10 @@
 package cracking.the.code.interview.trees_and_graphs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,115 +16,205 @@ public class Question4_7 {
 		String[][] dependencies = new String[][] { { "d", "a" }, { "b", "f" },
 				{ "d", "b" }, { "a", "f" }, { "c", "d" } };
 
-		List<String> order = createBuildOrder(projects, dependencies);
+		Project[] order = createBuildOrder(projects, dependencies);
 
-	}
-
-	private static List<String> createBuildOrder(String[] projects,
-			String[][] dependencies) {
-
-		Graph graph = createGraph(projects, dependencies);
-
-		List<String> buildOrder = new ArrayList<String>();
-
-		Map<String, Project> graphProjects = graph.getProjectMap();
-
-		while (buildOrder.size() < graphProjects.size()) {
-			for (Map.Entry<String, Project> entry : graphProjects.entrySet()) {
-
-				if (entry.getValue().getDependencies() == 0) {
-					buildOrder.add(entry.getKey());
-				}
-
+		if (order != null) {
+			for (Project p : order) {
+				System.out.println(p.getName() + " ");
 			}
 		}
 
-		return null;
 	}
 
-	public static Graph createGraph(String[] projects, String[][] dependencies) {
-		Graph graph = new Graph();
-		for (String name : projects) {
-			graph.createNode(name);
+	public static Project[] createBuildOrder(String[] projects,
+			String[][] dependencies) {
+		/**
+		 * Build a graph so that parent has references to children and child has
+		 * a number of parents.
+		 */
+		Graph graph = buildGraph(projects, dependencies);
+
+		/**
+		 * We have to use array because it is more convenient with this
+		 * algorithm. If we used list, we would have to swap elements adn that
+		 * would require removing plus adding elements to list at an index which
+		 * would result in O(n) time complexity.
+		 * 
+		 * Time complexity of this algorithm is O(n^2 + E) we could reduce time
+		 * complexity if we used iterator in addNonDependantProjects and remove
+		 * added project, but the big O would stay the same
+		 */
+		Project[] order = new Project[projects.length];
+
+		List<Project> projectList = new ArrayList<Project>(graph.getProjects()
+				.values());
+
+		int endOfArray = addNonDependantProjects(order, projectList, 0);
+		int nodesProcessed = 0;
+		while (nodesProcessed < order.length) { // we loop through this exactly
+												// n times
+			Project current = order[nodesProcessed];
+
+			if (current == null) {
+				return null;
+			}
+
+			/**
+			 * num of project * num of child depen. = num of edges in graph E
+			 */
+			for (Project child : current.getDependencies()) {
+				child.decreaseNumberOfDependencies();
+			}
+
+			current.setProcessed();
+			endOfArray++;
+
+			endOfArray = addNonDependantProjects(order, projectList, endOfArray); // n^2
+			nodesProcessed++;
 		}
 
+		return order;
+	}
+
+	private static int addNonDependantProjects(Project[] order,
+			List<Project> projectList, int endOfArray) {
+		for (Project project : projectList) {
+			if (project.getNumOfDenendencies() == 0 && !project.isProcessed()) {
+				order[endOfArray] = project;
+			}
+
+		}
+		return endOfArray;
+	}
+
+	/**
+	 * time complexity N^2*e = N*E
+	 * 
+	 * @param projects
+	 * @param dependencies
+	 * @return
+	 */
+	public static List<String> createBuildOrder2(String[] projects,
+			String[][] dependencies) {
+		Graph graph = buildGraph(projects, dependencies);
+
+		List<String> order = new ArrayList<String>();
+
+		List<Project> projectList = new ArrayList<Project>(graph.getProjects()
+				.values());
+
+		while (!projectList.isEmpty()) { // it will pass through all n projects
+											// in worst case
+			int size = projectList.size();
+			Iterator<Project> projectIterator = projectList.iterator();
+			while (projectIterator.hasNext()) { // it will explore remaining
+												// nodes
+				Project project = projectIterator.next();
+				if (project.getNumOfDenendencies() == 0) {
+					order.add(project.getName());
+					for (Project child : project.getDependencies()) { // for
+																		// each
+																		// edge
+						child.decreaseNumberOfDependencies();
+					}
+					projectIterator.remove();
+				}
+			}
+			if (projectList.size() == size) {
+				return null;
+			}
+		}
+
+		return order;
+	}
+
+	private static Graph buildGraph(String[] projects, String[][] dependencies) {
+		Graph graph = new Graph();
+		// add projects
+		for (String name : projects) {
+			Project project = new Project(name);
+			graph.addProject(project);
+		}
+
+		// add edges
 		for (String[] dependency : dependencies) {
-			String parent = dependency[0];
-			String child = dependency[1];
-			graph.createEdge(parent, child);
+			Project child = graph.getProject(dependency[0]);
+			Project parent = graph.getProject(dependency[1]);
+			child.increaseNumberOfDependencies();
+			parent.addDependency(child);
 		}
 
 		return graph;
-	}
-
-}
-
-class Graph {
-	/*
-	 * Why using an extra list when we can return map.getValues() ???
-	 */
-	// List<Project> projects = new ArrayList<Project>();
-	private Map<String, Project> projects = new HashMap<String, Project>();
-
-	public void createNode(String name) {
-		Project project = new Project(name);
-		if (!projects.containsKey(name))
-			projects.put(name, project);
-	}
-
-	public void createEdge(String parent, String child) {
-		Project par = projects.get(parent);
-		Project chi = projects.get(child);
-
-		par.addChild(chi);
-		par.setDependencies();
-
-	}
-
-	public List<Project> getProjectValues() {
-		return new ArrayList<>(projects.values());
-	}
-
-	public Map<String, Project> getProjectMap() {
-		return projects;
 	}
 }
 
 class Project {
 
-	private String name = "";
-	private Map<String, Project> children = new HashMap<String, Project>();
-	private int dependencies = 0;
+	private String name;
+	private List<Project> dependencies = new ArrayList<Project>();
+	private int numOfDependencies = 0;
+	private boolean processed;
 
 	public Project(String name) {
 		this.name = name;
 	}
 
-	public void addChild(Project child) {
-		if (!children.containsKey(child.name)) {
-			children.put(child.name, child);
-		}
-		return;
+	public boolean isProcessed() {
+		return processed;
+	}
 
+	public void setProcessed() {
+		this.processed = true;
+	}
+
+	public void addDependency(Project project) {
+		dependencies.add(project);
+	}
+
+	public void increaseNumberOfDependencies() {
+		numOfDependencies++;
+	}
+
+	public void decreaseNumberOfDependencies() {
+		numOfDependencies--;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public Map<String, Project> getChildren() {
-		return children;
+	public int getNumOfDenendencies() {
+		return numOfDependencies;
 	}
 
-	public List<Project> getChildrenList() {
-		return new ArrayList<>(children.values());
-	}
-
-	public int getDependencies() {
+	public List<Project> getDependencies() {
 		return dependencies;
 	}
 
-	public int setDependencies() {
-		return dependencies++;
+	@Override
+	public String toString() {
+		return name + " ";
+	}
+}
+
+class Graph {
+
+	private Map<String, Project> projects = new HashMap<String, Project>();
+
+	public Graph() {
+	}
+
+	public void addProject(Project project) {
+		if (!projects.containsKey(project.getName())) {
+			projects.put(project.getName(), project);
+		}
+	}
+
+	public Project getProject(String name) {
+		return projects.get(name);
+	}
+
+	public Map<String, Project> getProjects() {
+		return projects;
 	}
 }
---
+++ src/cracking/the/code/interview/trees_and_graphs/Question4_8.java
@@ -0,0 +1,98 @@
+package cracking.the.code.interview.trees_and_graphs;
+
+import cracking.the.code.interview.utilities.TreeNode;
+
+public class Question4_8 {
+
+	public static void main(String[] args) {
+		TreeNode root = createBinaryTree();
+		TreeNode nodeOne = root.getLeft().getLeft().getLeft();
+		TreeNode nodeTwo = root.getLeft().getRight();
+	//	TreeNode node = findFirstCommonAncestor(root, nodeOne, nodeTwo);
+		//System.out.println(node.getValue());
+		root = createBinaryTreeParentLinks();
+		nodeOne = root.getLeft().getLeft().getLeft();
+		nodeTwo = root.getLeft().getRight();
+		TreeNode node = findFirstCommonAncestorLinksToParent(root, nodeOne, nodeTwo);
+		System.out.println(node.getValue());
+	}
+
+	private static TreeNode findFirstCommonAncestor(TreeNode root,
+			TreeNode nodeOne, TreeNode nodeTwo) {
+
+		return null;
+	}
+
+	/**
+	 * Time complexity is O(d) where d is the max of two depth
+	 * @param root
+	 * @param nodeOne
+	 * @param nodeTwo
+	 * @return
+	 */
+	private static TreeNode findFirstCommonAncestorLinksToParent(TreeNode root,
+			TreeNode nodeOne, TreeNode nodeTwo) {
+		int depthNodeOne = depth(nodeOne);
+		int depthNodeTwo = depth(nodeTwo);
+		int delta = depthNodeOne - depthNodeTwo;
+		if(delta < 0) {
+			TreeNode pom = nodeOne;
+			nodeOne = nodeTwo;
+			nodeTwo = pom;
+			delta *= -1;
+		}
+
+		while(delta > 0) {
+			nodeOne = nodeOne.getParent();
+			delta--;
+		}
+		
+		while(nodeOne != null && nodeTwo != null && nodeOne != nodeTwo) {
+			nodeOne = nodeOne.getParent();
+			nodeTwo = nodeTwo.getParent();
+		}
+		
+		return nodeOne;
+	}
+
+	private static int depth(TreeNode node) {
+		int depth = 0;
+		while (node != null) {
+			node = node.getParent();
+			depth++;
+		}
+		return depth;
+	}
+
+	private static TreeNode createBinaryTree() {
+		TreeNode node3 = new TreeNode(3, null, null);
+		TreeNode node7 = new TreeNode(7, null, null);
+		TreeNode node5 = new TreeNode(3, node3, node7);
+		TreeNode node17 = new TreeNode(17, null, null);
+		TreeNode node15 = new TreeNode(15, null, node17);
+		TreeNode node10 = new TreeNode(10, node5, node15);
+		TreeNode node30 = new TreeNode(30, null, null);
+		TreeNode node20 = new TreeNode(20, node10, node30);
+		return node20;
+	}
+
+	private static TreeNode createBinaryTreeParentLinks() {
+		TreeNode node3 = new TreeNode(3, null, null);
+		TreeNode node7 = new TreeNode(7, null, null);
+		TreeNode node5 = new TreeNode(3, node3, node7);
+		node3.setParent(node5);
+		node7.setParent(node5);
+		TreeNode node17 = new TreeNode(17, null, null);
+		TreeNode node15 = new TreeNode(15, null, node17);
+		node17.setParent(node15);
+		TreeNode node10 = new TreeNode(10, node5, node15);
+		node5.setParent(node10);
+		node15.setParent(node10);
+		TreeNode node30 = new TreeNode(30, null, null);
+		TreeNode node20 = new TreeNode(20, node10, node30);
+		node10.setParent(node20);
+		node30.setParent(node20);
+		return node20;
+	}
+
+}
